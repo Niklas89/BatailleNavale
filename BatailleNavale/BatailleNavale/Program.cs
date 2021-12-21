806bc25664f9de7059848a6407cf2049e9834acb
Niklas <niklas_edelstam@hotmail.com>
2021-12-21 10:07:58 +0100

    modifs du code

--- BatailleNavale/BatailleNavale/Program.cs
+++ BatailleNavale/BatailleNavale/Program.cs
@@ -5,40 +5,6 @@ using System.IO;
 
 public class Program
 {
-	public static void Client()
-	{
-	connection:
-		try
-		{
-			// On créé un objet de type TcpClient défini par son host et son port
-			TcpClient client = new TcpClient("127.0.0.1", 1302);
-			//int nb = 2;
-			string text = "test";
-			string messageToSend = text; // Message à envoyer
-			int byteCount = Encoding.ASCII.GetByteCount(messageToSend + 1);
-			byte[] sendData = Encoding.ASCII.GetBytes(messageToSend);
-
-			//Envoi de message
-			NetworkStream stream = client.GetStream();
-			stream.Write(sendData, 0, sendData.Length);
-			Console.WriteLine("sending data to server...");
-
-			// Lecture de messages
-			StreamReader sr = new StreamReader(stream);
-			string response = sr.ReadLine();
-			Console.WriteLine(response);
-			AfficheMessage(response);
-
-			stream.Close();
-			client.Close();
-			Console.ReadKey();
-		}
-		catch (Exception)
-		{
-			Console.WriteLine("failed to connect...");
-			goto connection;
-		}
-	}
 	// Afficher le damier
 	public static void ShowBoard(char[,] board, char[] letters)
 	{
